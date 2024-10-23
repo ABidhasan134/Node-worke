41584fde2df6edcfcef0fa460213757f5c68c095
ABid hasan <abeydhasan134@gmail.com>
2024-10-24 01:59:22 +0600

    sortAnArrayObject

---
+++ sortArrayObject.js
@@ -0,0 +1,154 @@
+const result = [
+    {
+      "_id": "671941701ed982bd9a379dca",
+      "createAt": "2024-10-23T18:33:17.785Z",
+      "name": "Aabid Hasan",
+      "username": "lifeone1723",
+      "email": "lifeone1723@gmail.com",
+      "password": "",
+      "role": "general-user",
+      "level": "basic",
+      "createdQuizes": 0,
+      "rewards": 70,
+      "participatedQuizes": 7,
+      "image": "https://lh3.googleusercontent.com/a/ACg8ocLc-4Hd9dU7sV9fgjoI5331N67m-3hrDXfdEVvv9GguqgkXbw=s96-c"
+    },
+    {
+      "_id": "6716337a4ce5e28c0052a179",
+      "createAt": "2024-10-21T10:56:58.502Z",
+      "name": "Abrarul Hoque",
+      "username": "abrarcpi17",
+      "email": "abrarcpi17@gmail.com",
+      "password": "",
+      "role": "general-user",
+      "level": "basic",
+      "createdQuizes": 0,
+      "rewards": 60,
+      "participatedQuizes": 10,
+      "image": "https://lh3.googleusercontent.com/a/ACg8ocJTN21vFAQlXNYU0K-KNVQxxwPOLJHsU16Spl_zd2lIyiFza8k=s96-c"
+    },
+    {
+      "_id": "6712250e1e64dd6b212f12e9",
+      "createAt": "2024-10-18T09:06:20.537Z",
+      "username": "user",
+      "name": "Md Maruf Hossain",
+      "email": "user@user.com",
+      "password": "$2b$14$hOaFhl6IgXVZuSMcTXmdjuQvj.U44RtQ/Jc5nOEaABvWga5xq9IOi",
+      "role": "general-user",
+      "level": "pro",
+      "image": "https://avatar.iran.liara.run/public/boy?username=Ash",
+      "createdQuizes": 0,
+      "participatedQuizes": 55,
+      "rewards": 6
+    },
+    {
+      "_id": "67078f94e76e8a7d082fa985",
+      "createAt": "2024-10-10T08:25:56.344Z",
+      "name": "Abrarul Hoque",
+      "username": "jubairctg99",
+      "email": "jubairctg99@gmail.com",
+      "password": "",
+      "role": "general-user",
+      "level": "basic",
+      "createdQuizes": 0,
+      "participatedQuizes": 10,
+      "image": "https://lh3.googleusercontent.com/a/ACg8ocKXxzNIWRnq-DS7Zc_jBmbPTjni24XDuFp3bJR1DMX1ZRSsayc61w=s96-c",
+      "rewards": 54
+    },
+    {
+      "_id": "67056d2446aa10811ab88915",
+      "createAt": "2024-10-08T17:34:28.729Z",
+      "name": "Jahid Absar",
+      "username": "jahidabsar23",
+      "email": "jahidabsar23@gmail.com",
+      "password": "",
+      "role": "general-user",
+      "level": "basic",
+      "createdQuizes": 0,
+      "participatedQuizes": 40,
+      "image": "https://lh3.googleusercontent.com/a/ACg8ocJrMbNcOiNam6rLVzdxoP6RyZBy8R6kfYl4hMH7-e1eUy06-g=s96-c",
+      "rewards": 8
+    },
+    {
+      "_id": "66fa4bb3c0becde240f13db6",
+      "createAt": "2024-09-30T06:56:49.503Z",
+      "name": "Raisul Islam",
+      "username": "abeydhasan134",
+      "email": "abeydhasan134@gmail.com",
+      "password": "",
+      "role": "general-user",
+      "level": "basic",
+      "createdQuizes": 0,
+      "participatedQuizes": 6,
+      "image": "https://lh3.googleusercontent.com/a/ACg8ocK7_abmzYOVA1ACiPw9W-WUA9HzhLpMhUgIdIV5-BfT4R0ih-Om=s96-c",
+      "rewards": 1
+    },
+    {
+      "_id": "66f7ef4ba6f301004c0942cb",
+      "createAt": "2024-09-28T11:58:03.381Z",
+      "name": "Mahtabul Hoque Chowdhury",
+      "username": "mahach0396",
+      "email": "mahach0396@gmail.com",
+      "password": "",
+      "role": "general-user",
+      "level": "basic",
+      "createdQuizes": 0,
+      "participatedQuizes": 64,
+      "image": "https://lh3.googleusercontent.com/a/ACg8ocJrHeMnQzRyxpNJqta-rEo_SVU_ESet7OVauUwkkDsmrjlGMg=s96-c",
+      "rewards": 31
+    },
+    {
+      "_id": "66f6826553fee4209543a80e",
+      "createAt": "2024-09-27T10:01:09.028Z",
+      "username": "user",
+      "name": "user101",
+      "email": "user@m.com",
+      "password": "$2b$14$dpcCvb5Ap7mTxk75POuKYOYtcCnJxhS2IEUV.H/IIxsWMMu7WNuYa",
+      "role": "general-user",
+      "level": "basic",
+      "image": null,
+      "createdQuizes": 0,
+      "participatedQuizes": 46,
+      "rewards": 1364
+    },
+    {
+      "_id": "66f6710f08d9211ef8c09544",
+      "createAt": "2024-09-27T08:47:11.902Z",
+      "name": "rose",
+      "username": "r4750815",
+      "email": "r4750815@gmail.com",
+      "password": "",
+      "role": "general-user",
+      "level": "basic",
+      "createdQuizes": 0,
+      "participatedQuizes": 40,
+      "image": "https://lh3.googleusercontent.com/a/ACg8ocJ-lDzDqIA12pT8MSgRSJSsl2jA5I-rHMRTy4ZkThmoG3uq6A=s96-c",
+      "rewards": 6
+    },
+    {
+      "_id": "66f66f9e56ce15bd04bcb32c",
+      "createAt": "2024-09-27T08:41:01.937Z",
+      "username": "user1",
+      "name": "Md Maruf Hossain",
+      "email": "user1@user.com",
+      "password": "$2b$14$P1t1LqCWJim0zNUb6QFI6ua.3rT8wR6akqKmmt.aFa8RFNFn7HEAO",
+      "role": "general-user",
+      "level": "basic",
+      "image": null,
+      "createdQuizes": 0,
+      "participatedQuizes": 64,
+      "rewards": 45
+    }
+  ]
+
+  const sortArrayObjects = result.sort((a, b) => {
+    // Calculate the ratio for both a and b
+    const ratioA = a.participatedQuizes / (a.rewards || 1); // Prevent division by zero
+    const ratioB = b.participatedQuizes / (b.rewards || 1);
+  
+    // Sort in descending order based on the ratio
+    return ratioA - ratioB;
+  });
+  
+  console.log(sortArrayObjects);
+  
